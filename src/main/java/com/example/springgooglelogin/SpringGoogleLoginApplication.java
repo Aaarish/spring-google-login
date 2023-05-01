17c7c7c653f1a983655f2ea40ab152e1518196b4
Aarish <aarishm767@gmail.com>
2023-05-01 16:03:03 +0530

    Initial Commit

---
+++ src/main/java/com/example/springgooglelogin/SpringGoogleLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.springgooglelogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGoogleLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGoogleLoginApplication.class, args);
+	}
+
+}
